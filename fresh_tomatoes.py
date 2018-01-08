1cadc4825e4acbe50dfccf8646483c5f4c952997
Andre Tonelli <aveigas@br.ibm.com>
2018-01-08 11:28:27 -0200

    Editing page HTML

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -107,13 +107,15 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Fresh Tomatoes - Monty Python`s Movie Trailers</a>
           </div>
         </div>
       </div>
     </div>
     <div class="container">
-      {movie_tiles}
+      <div class="row row-eq-height">
+        {movie_tiles}
+      </div>
     </div>
   </body>
 </html>
@@ -123,8 +125,9 @@ main_page_content = '''
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_img_url}" width="220" height="342">
-    <h2>{title}</h2>
+    <img src="{poster_img_url}" width="198" height="308">
+    <h3>{year} - {title}</h3>
+    <p>{sinopse}</p>
 </div>
 '''
 
@@ -144,6 +147,8 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             title=movie.title,
+            year = movie.year,
+            sinopse = movie.sinopse,
             poster_img_url=movie.poster_img_url,
             trailer_youtube_id=trailer_youtube_id
         )
