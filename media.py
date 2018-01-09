2a100eca4342ab35eb98828658238a968865df7c
Andre Tonelli <aveigas@br.ibm.com>
2018-01-09 00:43:24 -0200

    Remove unnecessary code

--- media.py
+++ media.py
@@ -13,13 +13,8 @@ class Movie():
     """
     def __init__(self, title, year,
                  sinopse, poster_img_url, trailer_youtube_url):
-        self.title = title  # Movie name
-        self.year = year  # Movie premiere year
-        self.sinopse = sinopse  # A brief description of it
+        self.title = title
+        self.year = year
+        self.sinopse = sinopse
         self.poster_img_url = poster_img_url
         self.trailer_youtube_url = trailer_youtube_url
-
-
-# open youtube link and show the trailer
-def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
